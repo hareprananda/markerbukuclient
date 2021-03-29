d63135cd26a587363a71dbdc5a14e079ed93d047
hareprananda <52982157+hareprananda@users.noreply.github.com>
2021-03-29 23:33:28 +0800

    menambah component header

---
+++ component/pages/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = ({children}) => {
+    return (
+        <div className="headerContainer">
+            {children}
+        </div>
+    )
+}
+
+export default Header
